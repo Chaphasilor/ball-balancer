4411e0ab8bc1ab1355598dd335a743bb527be17f
Chaphasilor <ppp.u@web.de>
2019-12-23 19:34:23 +0100

    updated coordinates to reflect center of object

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ console.log(wCap);
 
 // segmenting by skin color (has to be adjusted)
 const skinColorUpper = hue => new cv.Vec(hue/2, 1 * 255, 1 * 255);
-const skinColorLower = hue => new cv.Vec(hue/2, 0.7 * 255, 0.4 * 255);
+const skinColorLower = hue => new cv.Vec(hue/2, 0.8 * 255, 0.4 * 255);
 // const skinColorUpper = hue => new cv.Vec(hue, 0.8 * 255, 0.6 * 255);
 // const skinColorLower = hue => new cv.Vec(hue, 0.1 * 255, 0.05 * 255);
 
 var counter = 0;
 
-const makeHandMask = (oldFrame) => {
+const trackBall = (oldFrame, modifyInput = false, locationFrame = null) => {
 
   const img = oldFrame.copy();
   // filter by skin color
-  let imgHLS = img.cvtColor(cv.COLOR_RGB2HSV);
+  const imgHLS = img.cvtColor(cv.COLOR_RGB2HSV);
   
   // if (counter > 350*2) {
   //   counter = 0;
@@ -27,15 +27,17 @@ const makeHandMask = (oldFrame) => {
   // const rangeMask = imgHLS.inRange(skinColorLower(startHue), skinColorUpper(startHue+10));
   // counter++;
   const rangeMask = imgHLS.inRange(skinColorLower(220), skinColorUpper(250));
+  // const rangeMask = imgHLS.inRange(skinColorLower(250), skinColorUpper(360));
   // const rangeMask = imgHLS.inRange(skinColorUpper(0), skinColorLower(30));
 
   // remove noise
   const blurred = rangeMask.blur(new cv.Size(10, 10));
-  const thresholded = blurred.threshold(
-    220,
-    255,
-    cv.THRESH_BINARY
-  );
+  // const thresholded = rangeMask.threshold(
+  //   220,
+  //   255,
+  //   cv.THRESH_BINARY
+  // );
+
 
   const contours = blurred.findContours(cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
   // console.log('contours:', contours);
@@ -44,14 +46,30 @@ const makeHandMask = (oldFrame) => {
       return largest.area < current.area ? current : largest;
     }, {area: 0})
     // let largestContour = contours[0];
-    let boundingRect = largestContour.boundingRect();
-    // console.log('largestContour:', largestContour);
-    // console.log('largestContour.boundingRect():', largestContour.boundingRect());
-    console.log('x: '+ boundingRect.x + ', y: ' +  boundingRect.y);
-    // console.log('blurred:', blurred);
-    // let bounded = blurred.drawRectangle(largestContour.boundingRect(), new cv.Vec(110, 255, 255), 5);
-    // modifies the actual input
-    let bounded = oldFrame.drawRectangle(boundingRect, new cv.Vec(0, 255, 0), 5);
+    if (largestContour != null) {
+
+      let x, y = 0;
+      let boundingRect = largestContour.boundingRect();
+      // console.log('largestContour:', largestContour);
+      // console.log('largestContour.boundingRect():', largestContour.boundingRect());
+      x = boundingRect.x + Math.round(boundingRect.width/2);
+      y = boundingRect.y + Math.round(boundingRect.height/2);
+      console.log('x: '+ x + ', y: ' +  y);
+      if (!modifyInput) {
+        blurred.set(y, x, [255]);
+        // console.log('blurred:', blurred);
+        let bounded = blurred.drawRectangle(largestContour.boundingRect(), new cv.Vec(110, 255, 255), 5);
+      } else {
+        if (locationFrame != null) {
+          locationFrame.set(y, x, [0, 255, 0]);
+        } else {
+          oldFrame.set(y, x, [255]);
+        }
+        // modifies the actual input
+        oldFrame.drawRectangle(boundingRect, new cv.Vec(0, 255, 0), 5);
+      }
+      
+    }
   } else {
     return blurred;
   }
@@ -94,11 +112,11 @@ while (!done) {
   }
 
   // console.log('frame: ', frame);
-  let masked = makeHandMask(frame);
-  let modified = modifyFrame(frame);
-  // console.log('masked:', masked);
+  let location = new cv.Mat(frame.rows, frame.cols, cv.CV_8UC3, [0, 0, 0]);
+  let masked = trackBall(frame, true, location);
   cv.imshow('frame', frame);
   // cv.imshow('masked', masked);
+  // cv.imshow('location', location);
   // cv.imshow('modified', modified);
   // cv.waitKey();
 
